17f3a75b359a8a98aafcd8007e51480baeb87da0
pheralb <pablohdez4340@gmail.com>
2023-02-03 15:29:27 +0000

    🛠️ Update next.config.

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+!process.env.SKIP_ENV_VALIDATION && (await import("./src/env/schema.mjs"));
+
+/**
+ * @template {import('next').NextConfig} T
+ * @param {T} config - A generic parameter that flows through to the return type
+ * @constraint {{import('next').NextConfig}}
+ */
+function defineNextConfig(config) {
+  return config;
+}
+
+export default defineNextConfig({
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
+});
