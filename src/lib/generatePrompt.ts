dfb7c999fbddaa8faa66360eb7790a69fa6bd02b
David Huertas <davidhuertasortiz@gmail.com>
2023-02-03 22:01:35 +0100

    feat: add front
    Co-authored-by: Nacho Aldama <sr.drabx@gmail.com>

--- src/lib/generatePrompt.ts
+++ src/lib/generatePrompt.ts
@@ -1,10 +1,31 @@
-export type Product = {
-	body: string;
-	originalBody: string;
-	title: string;
-	rating: number;
-};
+export interface Product {
+  body: string;
+  originalBody: string;
+  title: string;
+  rating: number;
+  classification: Classification;
+  screenshot: string;
+}
 
-export function generatePrompt(category: string, product: Product) {
-	return `${product.body} [${category} ${product.rating} stars]`;
+export interface Classification {
+  label: string;
+  confidence: number;
+  labels: Labels;
+}
+
+export interface Labels {
+  CG: Cg;
+  OR: Or;
+}
+
+export interface Cg {
+  confidence: number;
+}
+
+export interface Or {
+  confidence: number;
+}
+
+export function generatePrompt(category: string, product: Partial<Product>) {
+  return `${product.body} [${category} ${product.rating} stars]`;
 }
