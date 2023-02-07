39e5530899b2b55dbddaa51e1feaad910d5689b4
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2023-02-07 14:13:17 +0100

    Fix railway port

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -7,6 +7,8 @@ dotenv.config();
 
 const port = process.env.PORT || 3001;
 
+console.log(process.env.PORT);
+
 app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
@@ -16,6 +18,6 @@ app.get("/scrapper/:url", (req, res) => {
 	return scrapper(url, res);
 });
 
-app.listen(process.env.port, () => {
+app.listen(port, () => {
 	console.log(`Server started on port ${port}`);
 });
